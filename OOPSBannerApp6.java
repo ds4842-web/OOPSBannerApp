3daa5fdf032fe9c7258ff8582473b33675e0e0be
ds4842-web <ds4842@srmist.edu.in>
2026-02-24 15:04:29 +0530

    Created a new branch UC-6 and added the new file OOPSBannerApp6 into the new branch

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,58 @@
+public class OOPSBannerApp6 {
+
+    // O pattern
+    public static String[] getOPattern() {
+        return new String[]{
+            " ****** ",
+            "**    **",
+            "**    **",
+            "**    **",
+            "**    **",
+            "**    **",
+            " ****** "
+        };
+    }
+
+    // P pattern
+    public static String[] getPPattern() {
+        return new String[]{
+            " ****** ",
+            "**    **",
+            "**    **",
+            " ****** ",
+            "**      ",
+            "**      ",
+            "**      "
+        };
+    }
+
+    // S pattern
+    public static String[] getSPattern() {
+        return new String[]{
+            " ****** ",
+            "**      ",
+            "**      ",
+            " ****** ",
+            "      **",
+            "      **",
+            " ****** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] o = getOPattern();
+        String[] p = getPPattern();
+        String[] s = getSPattern();
+
+        // Print O O P S
+        for (int i = 0; i < o.length; i++) {
+            System.out.println(
+                o[i] + "   " +
+                o[i] + "   " +
+                p[i] + "   " +
+                s[i]
+            );
+        }
+    }
+}
